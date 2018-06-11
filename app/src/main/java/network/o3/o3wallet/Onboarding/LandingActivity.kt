f2671b39a28aba1fec4aff5c76fee60d535ccd1a
SaltySkip <terentiev.andrei@gmail.com>
2018-06-11 11:11:14 +0900

    landing comemcnes

---
+++ app/src/main/java/network/o3/o3wallet/Onboarding/LandingActivity.kt
@@ -0,0 +1,13 @@
+package network.o3.o3wallet.Onboarding
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import network.o3.o3wallet.R
+
+class LandingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.onboarding_landing_activity)
+    }
+}
