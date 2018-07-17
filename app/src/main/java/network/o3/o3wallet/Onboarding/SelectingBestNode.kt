9d62e121b466162f4d52efa4ef0866f538fd79ea
SaltySkip <terentiev.andrei@gmail.com>
2018-07-17 10:50:13 +0900

    consolidate theme

--- app/src/main/java/network/o3/o3wallet/Onboarding/SelectingBestNode.kt
+++ app/src/main/java/network/o3/o3wallet/Onboarding/SelectingBestNode.kt
@@ -2,7 +2,9 @@ package network.o3.o3wallet.Onboarding
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import kotlinx.coroutines.experimental.Deferred
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.async
@@ -15,6 +17,7 @@ import network.o3.o3wallet.PersistentStore
 import network.o3.o3wallet.R
 import org.jetbrains.anko.coroutines.experimental.bg
 import org.jetbrains.anko.defaultSharedPreferences
+import org.jetbrains.anko.find
 
 class SelectingBestNode : AppCompatActivity() {
 
@@ -22,6 +25,9 @@ class SelectingBestNode : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.portfolio_activity_selecting_best_node)
         val sharedPref = O3Wallet.appContext!!.defaultSharedPreferences
+        supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        supportActionBar?.setCustomView(R.layout.actionbar_layout)
+        find<TextView>(R.id.mytext).text = resources.getString(R.string.WALLET_my_o3_wallet)
         with (sharedPref.edit()) {
             putBoolean("USING_PRIVATE_NET", false)
             commit()
