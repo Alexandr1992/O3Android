9fbe68653bb2504c27234f7931c9c19d52dc3533
SaltySkip <terentiev.andrei@gmail.com>
2018-07-17 17:40:31 +0900

    add-progress-on-dapp

--- app/src/main/java/network/o3/o3wallet/Dapp/DAppBrowserActivity.kt
+++ app/src/main/java/network/o3/o3wallet/Dapp/DAppBrowserActivity.kt
@@ -1,5 +1,6 @@
 package network.o3.o3wallet.Dapp
 
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,7 +9,8 @@ import android.webkit.WebView
 import android.webkit.JsResult
 import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
-
+import android.widget.ProgressBar
+import org.jetbrains.anko.find
 
 
 class DAppBrowserActivity : AppCompatActivity() {
@@ -23,7 +25,23 @@ class DAppBrowserActivity : AppCompatActivity() {
 
         view = findViewById<View>(R.id.dapp_browser_root_layout)
         webView = view.findViewById(R.id.dapp_browser_webview)
+        val webLoader = find<ProgressBar>(R.id.webLoader)
         val url = intent.getStringExtra("url")
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                // do your handling codes here, which url is the requested url
+                // probably you need to open that url rather than redirect:
+                view.loadUrl(url)
+                return false // then it is not handled by default action
+            }
+
+            override fun onPageCommitVisible(view: WebView?, url: String?) {
+                super.onPageCommitVisible(view, url)
+                webLoader.visibility = View.GONE
+            }
+        }
+
         webView.loadUrl(url)
         val webSettings = webView.settings
         webSettings.javaScriptEnabled = true
