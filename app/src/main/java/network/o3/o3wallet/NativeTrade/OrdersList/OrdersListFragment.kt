9d45c89623f9b33ef8a14ce02a41e7a8fdf88cb7
SaltySkip <terentiev.andrei@gmail.com>
2018-09-10 10:36:46 +0900

    continuiung

--- app/src/main/java/network/o3/o3wallet/NativeTrade/OrdersList/OrdersListFragment.kt
+++ app/src/main/java/network/o3/o3wallet/NativeTrade/OrdersList/OrdersListFragment.kt
@@ -3,33 +3,60 @@ package network.o3.o3wallet.NativeTrade.OrdersList
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.LinearLayoutManager.VERTICAL
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.wallet_fragment_account.*
 import network.o3.o3wallet.API.Switcheo.SwitcheoAPI
 import network.o3.o3wallet.API.Switcheo.SwitcheoOrders
 import network.o3.o3wallet.NativeTrade.NativeTradeBaseAssetBottomSheet
 import network.o3.o3wallet.R
 import network.o3.o3wallet.RoundedBottomSheetDialogFragment
+import network.o3.o3wallet.getColorFromAttr
 import org.jetbrains.anko.find
+import org.jetbrains.anko.support.v4.onUiThread
 
 class OrdersListFragment : Fragment() {
+    private lateinit var mView: View
+
+    private lateinit var ordersListView: RecyclerView
+    private lateinit var swipeContainer: SwipeRefreshLayout
+
+    fun loadOrders() {
+        SwitcheoAPI().getPendingOrders {
+            onUiThread {
+                ordersListView.adapter = OrdersAdapter(it.first!!, this)
+                swipeContainer.isRefreshing = false
+            }
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.native_trade_orders_list_fragment, container, false)!!
-        val ordersListView = view.find<RecyclerView>(R.id.ordersListView)
+        mView = inflater.inflate(R.layout.native_trade_orders_list_fragment, container, false)!!
+        ordersListView = mView.find<RecyclerView>(R.id.ordersListView)
         val recyclerLayout = LinearLayoutManager(context)
         recyclerLayout.orientation = VERTICAL
         ordersListView.layoutManager = recyclerLayout
-        SwitcheoAPI().getPendingOrders {
-            ordersListView.adapter = OrdersAdapter(it.first!!, this)
+        loadOrders()
+
+        swipeContainer = mView.find<SwipeRefreshLayout>(R.id.orderSwipeContainer)
+        swipeContainer.setColorSchemeResources(R.color.colorPrimary,
+                R.color.colorPrimary,
+                R.color.colorPrimary,
+                R.color.colorPrimary)
+
+        swipeContainer.setProgressBackgroundColorSchemeColor(context!!.getColorFromAttr(R.attr.secondaryBackgroundColor))
+        swipeContainer.setOnRefreshListener {
+            loadOrders()
         }
-        return view
+
+        return mView
     }
 
     fun showOrderUpdateOptions(order: SwitcheoOrders) {
--- app/src/main/java/network/o3/o3wallet/Wallet/AccountFragmentPagerAdapter.kt
+++ app/src/main/java/network/o3/o3wallet/Wallet/AccountFragmentPagerAdapter.kt
@@ -3,6 +3,7 @@ package network.o3.o3wallet.Wallet
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.FragmentPagerAdapter
+import network.o3.o3wallet.NativeTrade.OrdersList.OrdersListFragment
 import network.o3.o3wallet.R
 import network.o3.o3wallet.Settings.ContactsFragment
 import network.o3.o3wallet.Wallet.TransactionHistory.TransactionHistoryFragment
@@ -12,9 +13,9 @@ import network.o3.o3wallet.Wallet.TransactionHistory.TransactionHistoryFragment
  */
 class AccountFragmentPagerAdapter(fm: android.support.v4.app.FragmentManager, context: Context) : FragmentPagerAdapter(fm){
 
-    private val PAGE_COUNT = 2
+    private val PAGE_COUNT = 3
     private val tabTitles = arrayOf(context.resources.getString(R.string.WALLET_assets),
-            context.resources.getString(R.string.WALLET_transactions))
+            context.resources.getString(R.string.WALLET_transactions), context.resources.getString(R.string.NATIVE_TRADE_orders))
     private val context: Context = context
 
 
@@ -25,8 +26,10 @@ class AccountFragmentPagerAdapter(fm: android.support.v4.app.FragmentManager, co
     override fun getItem(position: Int): android.support.v4.app.Fragment {
         if (position == 0) {
             return AccountFragment.newInstance()
-        } else {
+        } else if (position == 1) {
             return TransactionHistoryFragment.newInstance()
+        } else {
+            return OrdersListFragment.newInstance()
         }
     }
 
