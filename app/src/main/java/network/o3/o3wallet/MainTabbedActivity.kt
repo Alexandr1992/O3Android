9d9e85378849951689342b449c01ae5ccda193d2
SaltySkip <terentiev.andrei@gmail.com>
2018-10-09 10:45:52 +0900

    remove amp on debug

--- app/src/main/java/network/o3/o3wallet/MainTabbedActivity.kt
+++ app/src/main/java/network/o3/o3wallet/MainTabbedActivity.kt
@@ -133,9 +133,9 @@ class MainTabbedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tabbar_activity_main_tabbed)
-       // if (!BuildConfig.DEBUG) {
+        if (!BuildConfig.DEBUG) {
             Amplitude.getInstance().initialize(this, resources.getString(R.string.Amplitude_API_Key)).enableForegroundTracking(application)
-        //}
+        }
 
         val selectedFragment = fragments!!.get(0)
         val transaction = supportFragmentManager.beginTransaction()
