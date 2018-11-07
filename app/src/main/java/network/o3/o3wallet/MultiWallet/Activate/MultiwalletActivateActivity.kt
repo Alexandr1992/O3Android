5a82da9ad296d591eba3917daa59b83f97e93e1c
SaltySkip <terentiev.andrei@gmail.com>
2018-11-07 12:01:14 +0900

    working on multiwallet

---
+++ app/src/main/java/network/o3/o3wallet/MultiWallet/Activate/MultiwalletActivateActivity.kt
@@ -0,0 +1,12 @@
+package network.o3.o3wallet.MultiWallet.Activate
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MultiwalletActivateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.multiwallet_activate_activity)
+    }
+}
---
+++ app/src/main/java/network/o3/o3wallet/NEP6.kt
@@ -0,0 +1,7 @@
+package network.o3.o3wallet.Wallet
+
+import com.github.salomonbrys.kotson.fromJson
+import com.google.gson.Gson
+import network.o3.o3wallet.O3Wallet
+import java.io.File
+
