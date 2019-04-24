64f734992a5d3a1564f576290749128799e4ccd0
SaltySkip <terentiev.andrei@gmail.com>
2019-04-24 15:25:53 +0900

    working on inbox

---
+++ app/src/main/java/network/o3/o3wallet/Inbox/InboxRootActivity.kt
@@ -0,0 +1,12 @@
+package network.o3.o3wallet.Inbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InboxRootActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inbox_root_activity)
+    }
+}
